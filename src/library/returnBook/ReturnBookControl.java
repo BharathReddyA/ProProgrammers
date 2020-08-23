b4efaf642a91ce3d0288d0b2c5dbd94bc74cc731
Amin0710 <68992738+Amin0710@users.noreply.github.com>
2020-08-23 22:53:50 +1000

    Update ReturnBookControl.java

--- src/library/returnBook/ReturnBookControl.java
+++ src/library/returnBook/ReturnBookControl.java
@@ -54,8 +54,7 @@ public class ReturnBookControl {
 	public void bookScanned(int bookId){
 		//if (!sTaTe.equals(cOnTrOl_sTaTe.READY)) 
 		if (!state.equals(ControlState.READY)) 
-			//throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");
-			{
+			//throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");{
 			throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");
 			}
 		
