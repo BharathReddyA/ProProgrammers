e36f2258f7c9d5d61480198cc516ab01940abe44
shubham635 <68891328+shubham635@users.noreply.github.com>
2020-08-28 18:49:18 +1000

    Update PayFineUI.java

--- src/library/payfine/PayFineUI.java
+++ src/library/payfine/PayFineUI.java
@@ -5,35 +5,34 @@ import java.util.Scanner;
 public class PayFineUI {
 
 
-	public static enum UI_State { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
-	//review//UI_State to UIState
+	public static enum UIState { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
 
-	private Pay_Fine_Control Control; //review//Pay_Fine_Control to payFineControl and Control to control
+	private payFineControl control; 
 	private Scanner input;
-	private UI_State State;//review//UI_State to UIState
+	private UIState State;
 
 	
 
-	public PayFineUI(Pay_Fine_Conrol Control) {//review//Pay_Fine_Control to payFineControl and Control to control
-		this.Control = Control; //review// Control to control twice
+	public PayFineUI(payFineConrol control) 
+		this.control = control; 
 		input = new Scanner(System.in);
-		State = UI_State.INITIALISED;//review//UI_State to UIState
-		Control.Set_UI(this);//review//Set_UI to setUI and Control to control
+		State = UIState.INITIALISED;
+		control.setUI(this);
 	}
 	
 	
-	public void Set_State(UI_State State) { //review//UI_State to UIState and Set_State to setState
+	public void setState(UIState State) { 
 
-	public PayFineUI(Pay_Fine_Control Control) {//review//Pay_Fine_Control to payFineControl and Control to control
-		this.Control = Control;//review// Control to control twice
+	public PayFineUI(payFineControl control) {
+		this.control = control;
 		input = new Scanner(System.in);
-		State = UI_State.INITIALISED;//review//UI_State to UIState
-		Control.Set_UI(this); //review//Set_UI to setUI and Control to control
+		State = UIState.INITIALISED;
+		control.setUI(this); 
 	}
 	
 	
-	public void Set_State(UI_State State) {//review//UI_State to UIState and Set_State to setState
-		this.State = State; //review//State to state twice
+	public void setState(UIState State) {
+		this.state = state; 
 	}
 
 
@@ -42,17 +41,17 @@ public class PayFineUI {
 		
 		while (true) {
 			
-			switch (State) {//review//State to state 
+			switch (state) { 
 			
 			case READY:
-				String Mem_Str = input("Swipe member card (press <enter> to cancel): "); //review//Mem_Str to memberString
-				if (Mem_Str.length() == 0) {//review//Mem_Str to memberString
-					Control.Cancel();//review//Control to control
+				String memberString = input("Swipe member card (press <enter> to cancel): ");
+				if (memberString.length() == 0) {
+					control.Cancel();
 					break;
 				}
 				try {
-					int Member_ID = Integer.valueOf(Mem_Str).intValue();//review//Member_ID to memberId and Mem_Str to memberString
-					Control.Card_Swiped(Member_ID);//review//Card_Swiped to cardSwiped and Member_ID to memberId and Control to control
+					int memberId = Integer.valueOf(memberString).intValue();
+					control.cardSwiped(memberId);
 				}
 				catch (NumberFormatException e) {
 					output("Invalid memberId");
@@ -61,20 +60,20 @@ public class PayFineUI {
 				
 			case PAYING:
 				double Amount = 0;
-				String Amt_Str = input("Enter amount (<Enter> cancels) : ");//review//Amt_Str to amountString
-				if (Amt_Str.length() == 0) {//review//Amt_Str to amountString
-					Control.Cancel();//review//Control to control
+				String amountString = input("Enter amount (<Enter> cancels) : ");
+				if (amountString.length() == 0) {
+					control.Cancel();
 					break;
 				}
 				try {
-					Amount = Double.valueOf(Amt_Str).doubleValue();//review//Amt_Str to amountString
+					Amount = Double.valueOf(amountString).doubleValue();
 				}
 				catch (NumberFormatException e) {}
 				if (Amount <= 0) {
 					output("Amount must be positive");
 					break;
 				}
-				Control.Pay_Fine(Amount);//review//Control to control and Pay_Fine to payFine
+				control.payFine(Amount);
 				break;
 								
 			case CANCELLED:
@@ -87,7 +86,7 @@ public class PayFineUI {
 			
 			default:
 				output("Unhandled state");
-				throw new RuntimeException("FixBookUI : unhandled state :" + State);//review//State to state 		
+				throw new RuntimeException("FixBookUI : unhandled state :" + state); 		
 			
 			}		
 		}		
